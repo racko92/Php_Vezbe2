85336911f4a2a9cc5461ff6f2edb90db4edf833e
racko92 <vladimir.rackovic.92@gmail.com>
2017-10-10 16:49:19 +0200

    Created Login and Register forms

--- login.php
+++ login.php
@@ -4,6 +4,19 @@
 ?>
 
 <main class="main">
+	<form class="form" method="POST">
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="email" name="email">
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="form-submit">
+			<button type="submit">Login</button>
+		</div>
+	</form>
 </main>
 
 <?php 
--- register.php
+++ register.php
@@ -4,6 +4,28 @@
 ?>
 
 <main class="main">
+
+	<form class="form" method="POST">
+		<div class="form-group">
+			<label for="firstName">First Name</label>
+			<input type="text" name="firstName">
+		</div>
+		<div class="form-group">
+			<label for="lastName">Last Name</label>
+			<input type="text" name="lastName">
+		</div>
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="email" name="email">
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="form-submit">
+			<button type="submit">Register</button>
+		</div>
+	</form>
 </main>
 
 <?php 
